71c1618b1b94c58dfeb84423bf962c92c9a97751
Soul-inside <soul_inside@mail.ru>
2014-12-05 14:17:19 +0300

    gvkhgk

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿// Last Change: 2014 12 04 16:49
+
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace WpfApplication1
+{
+	/// <summary>
+	///     Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private readonly Polyline Pl_green = new Polyline {Stroke = Brushes.Green, StrokeThickness = 2, FillRule = FillRule.EvenOdd};
+		private readonly SquareLine SLine = new SquareLine {Stroke = Brushes.SpringGreen, StrokeThickness = 2, StrokeDashArray = new DoubleCollection {2}};
+		private readonly Line tmpLine = new Line {Stroke = Brushes.SpringGreen, StrokeThickness = 2, StrokeDashArray = new DoubleCollection {2}};
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			CnvDraw.Children.Add(SLine);
+			CnvDraw.Children.Add(Pl_green);
+			CnvDraw.Children.Add(tmpLine);
+		}
+
+		private void ResizeImage()
+		{
+			ImgWell.MaxHeight = W.Height;
+			ImgWell.MaxWidth = W.Width;
+			CnvDraw.Height = ImgWell.Height;
+			CnvDraw.Width = ImgWell.Width;
+		}
+
+		private void CnvDraw_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (Pl_green.Points.Count() > 1)
+			{
+				SLine.EndPoint = new Point(e.GetPosition(CnvDraw).X, e.GetPosition(CnvDraw).Y);
+			}
+			else if (Pl_green.Points.Count() > 0)
+			{
+				tmpLine.X2 = tmpLine.X1;
+				tmpLine.Y2 = e.GetPosition(CnvDraw).Y;
+			}
+		}
+
+		private void CnvDraw_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			if (CnvDraw.Children.Contains(SLine))
+			{
+				if (!Pl_green.Points.Any())
+				{
+					Pl_green.Points.Add(new Point(e.GetPosition(CnvDraw).X, e.GetPosition(CnvDraw).Y));
+					tmpLine.X1 = e.GetPosition(CnvDraw).X;
+					tmpLine.Y1 = e.GetPosition(CnvDraw).Y;
+				}
+				else if (Pl_green.Points.Count() < 2)
+				{
+					Pl_green.Points.Add(new Point(tmpLine.X1, e.GetPosition(CnvDraw).Y));
+					SLine.StartPoint = new Point(tmpLine.X1, e.GetPosition(CnvDraw).Y);
+					SLine.EndPoint = new Point(tmpLine.X1, e.GetPosition(CnvDraw).Y);
+					CnvDraw.Children.Remove(tmpLine);
+				}
+				else
+				{
+					Pl_green.Points.Add(new Point(e.GetPosition(CnvDraw).X, SLine.StartPoint.Y));
+					Pl_green.Points.Add(new Point(e.GetPosition(CnvDraw).X, e.GetPosition(CnvDraw).Y));
+					SLine.StartPoint = SLine.EndPoint;
+				}
+			}
+		}
+
+		private void CnvDraw_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			CnvDraw.Children.Remove(SLine);
+		}
+
+		private void BtImageLoad_Click(object sender, RoutedEventArgs e)
+		{
+			var dlg = new OpenFileDialog
+			{
+				FileName = " Image ",
+				DefaultExt = ".jpg",
+				Filter = " Image (.jpg)|*.jpg"
+			};
+			bool? result = dlg.ShowDialog();
+			if (result == true)
+			{
+				try
+				{
+					string filename = dlg.FileName;
+					var bi3 = new BitmapImage();
+					bi3.BeginInit();
+					bi3.UriSource = new Uri(filename, UriKind.Absolute);
+					bi3.EndInit();
+					ImgWell.Source = bi3;
+					ResizeImage();
+				}
+				catch (NotSupportedException)
+				{
+					MessageBox.Show("Некорректный тип файла!", "Внимание");
+				}
+			}
+		}
+
+		private void BtGreen_Click(object sender, RoutedEventArgs e)
+		{
+		}
+
+		private void BtRed_Click(object sender, RoutedEventArgs e)
+		{
+		}
+
+		private void TgBtOrange_Checked(object sender, RoutedEventArgs e)
+		{
+		}
+	}
+}
---
+++ WpfApplication1/SquareLine.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+	public class SquareLine : Shape
+	{
+		StreamGeometry geometry = new StreamGeometry();
+
+		// Dependency properties
+		public static readonly DependencyProperty StartPointProperty =
+			LineGeometry.StartPointProperty.AddOwner(
+				typeof(SquareLine),
+				new FrameworkPropertyMetadata(new Point(0, 0),
+					FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+		public static readonly DependencyProperty EndPointProperty =
+			LineGeometry.EndPointProperty.AddOwner(
+				typeof(SquareLine),
+				new FrameworkPropertyMetadata(new Point(0, 0),
+					FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+		public Point StartPoint
+		{
+			set { SetValue(StartPointProperty, value); }
+			get { return (Point)GetValue(StartPointProperty); }
+		}
+
+		public Point EndPoint
+		{
+			set { SetValue(EndPointProperty, value); }
+			get { return (Point)GetValue(EndPointProperty); }
+		}
+
+		protected override Geometry DefiningGeometry
+		{
+			get
+			{
+				using (StreamGeometryContext ctx = geometry.Open())
+				{
+					ctx.BeginFigure(StartPoint, true, false);
+					ctx.LineTo(new Point(EndPoint.X, StartPoint.Y), true, false);
+					ctx.LineTo(EndPoint, true, false);
+				}
+				return geometry; 
+			}
+		}
+	}
+}
